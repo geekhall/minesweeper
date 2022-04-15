2afbfe444e0d8f63f3bd1ca9b90f46ccd8dfb962
geekhall <174821300@qq.com>
2022-04-15 18:41:43 +0800

    feat: add confetti

---
+++ src/components/Confetti.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+// pnpm install --save canvas-confetti
+
+import confetti from 'canvas-confetti'
+// import {isPassed} from '~/state'
+
+const props = defineProps < {
+  passed: boolean
+}>()
+
+function congrats() {
+  const defaults = {
+    colors: [
+      '#5D8C7B',
+      '#F3D090',
+      '#F2A678',
+      '#D9695D',
+      '#8C4545',
+    ],
+    shapes: ['square'],
+    ticks: 500,
+  } as confetti.Options
+  confetti({
+    ...defaults,
+    particalCount: 100,
+    spread: 100,
+    origin: { y: 0 },
+  })
+  setTimeout(() => {
+    confetti({
+      ...defaults,
+      particalCount: 50,
+      angle: 60,
+      spread: 80,
+      origin: { x: 0 },
+    })
+  }, 250)
+  setTimeout(() => {
+    confetti({
+      ...defaults,
+      particalCount: 50,
+      angle: 120,
+      spread: 80,
+      origin: { x: 1 },
+    })
+  }, 400)
+}
+
+watch(() => props.passed, (v) => {
+  if (v)
+    setTimeout(congrats, 300)
+}, { flush: 'post' })
+
+</script>
+
+<template>
+  <div />
+</template>
+
+<style lang="sass" scoped>
+</style>
